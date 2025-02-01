80868b66196515fb283e178d6201533a6dbf4419
Tomás Sasovsky <to.sasovsky@gmail.com>
2025-02-01 19:08:06 +0100

    refactor: remove unused runBlocking import from CommentApiImpl.kt

--- packages/comments_repository/android/src/main/kotlin/com/example/verygoodcore/CommentApiImpl.kt
+++ packages/comments_repository/android/src/main/kotlin/com/example/verygoodcore/CommentApiImpl.kt
@@ -5,7 +5,6 @@ import CommentModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import java.io.BufferedReader
