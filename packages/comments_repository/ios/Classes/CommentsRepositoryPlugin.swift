24605d0319458f47b968379e0139b0202a185468
Tomás Sasovsky <to.sasovsky@gmail.com>
2025-02-01 18:36:14 +0100

    refactor: remove unused comment model and clean up iOS plugin registration

--- packages/comments_repository/ios/Classes/CommentsRepositoryPlugin.swift
+++ packages/comments_repository/ios/Classes/CommentsRepositoryPlugin.swift
@@ -3,15 +3,6 @@ import UIKit
 
 public class CommentsRepositoryPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    // Existing MethodChannel registration (if you still need it)
-    let channel = FlutterMethodChannel(name: "comments_repository_ios", binaryMessenger: registrar.messenger())
-    let instance = CommentsRepositoryPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-    
     CommentApiSetup.setUp(binaryMessenger: registrar.messenger(), api: CommentApiImpl())
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS")
   }
 }
